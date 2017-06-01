1573bf151feb08cff8070328bbe30a849d498719
familyman-131 <familyman-131@users.noreply.github.com>
2017-06-01 13:03:38 +0300

    Update autoban.sh

--- autoban.sh
+++ autoban.sh
@@ -5,32 +5,36 @@
 #incrontab -l
 #/path/to/folder IN_CLOSE_WRITE /path/to/autoban.sh $@/$#
 ####################
-#LOG=/tmp/result.txt
+DATE=`date +%Y-%m-%d:%H:%M:%S`
+#LOG=/path/to/debug.log
 INCRON_FILE_NAME=$(echo "$1")
 LIST=$(cat ${INCRON_FILE_NAME})
 ####################
 # http://linuxru.org/linux/324
 #ipset -F autoban
-#INCRON_FILE_NAME=autoban
-#echo "OK" >> /tmp/result.txt
+#echo "OK" >> ${LOG}
 #echo "${INCRON_FILE_NAME}" >> ${LOG}
 #echo "${LIST}" >> ${LOG}
 ####################
-if [[ -z ${LIST} ]]
+echo "${INCRON_FILE_NAME}"  | grep -q purge.autoban
+FILE_EXT_CHK=$(echo "$?")
+if [ ${FILE_EXT_CHK} = 0 ]
     then
-    echo "file empty `date`"  #>> ${LOG}
+    echo "file empty ${DATE}"  #>> ${LOG}
     ipset -F autoban
+#    echo "" > /path/to/autobannediplist.txt
+    rm ${INCRON_FILE_NAME}
     exit
     else
-    echo "file full `date`"  #>> ${LOG}
+    echo "file full ${DATE}"  #>> ${LOG}
 fi
 ########
 ipset create autoban hash:ip hashsize 131072 maxelem 2000000
 for ipaddr in ${LIST}
 do
   ipset -A autoban $ipaddr
-#  echo "$ipaddr" >> ${LOG}
+#  echo "$ipaddr  ${DATE}" >> /path/to/autobannediplist.txt
 done
-
-#rm ${INCRON_FILE_NAME}
+########
+rm ${INCRON_FILE_NAME}
 ###################
